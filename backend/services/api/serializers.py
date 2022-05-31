223e9e5ad9a02f480b12491406a6a9741a6808f1
roger <rher92@gmail.com>
2022-05-31 14:57:48 +0200

    endpoint to create service, list, and retireve

--- backend/services/api/serializers.py
+++ backend/services/api/serializers.py
@@ -1,9 +1,51 @@
 from rest_framework import serializers
 
-from backend.services.models import Articulos, TiposGestion, Material, Expediente
+from backend.services.models import Articulos, TiposGestion, Material, Expediente, ExpedientesImagenes
+from backend.users.models import Cliente
+
+class ExpedienteSerializer(serializers.Serializer):
+    identificador = serializers.CharField()  # expediente
+    gestion = serializers.CharField()  # TiposGestion
+    material = serializers.CharField(required=False)  # Material
+    precio = serializers.CharField(required=False)
+    articulos = serializers.CharField(required=False)  # Articulos
+    file = serializers.FileField()
+
+    def validate_tipo_gestion(self, data):
+        try:
+            tipo_gestion = TiposGestion.objects.get(pk=data)
+        except TiposGestion.DoesNotExist:
+            raise serializers.ValidationError(f'tipo de gestion with pk: {data} does not exists.')
+        return tipo_gestion
+
+    def validate_material(self, data):
+        try:
+            material = Material.objects.get(pk=data)
+        except Material.DoesNotExist:
+            raise serializers.ValidationError(f'Material with pk: {data} does not exists.')
+        return material
+
+    def validate_articulo(self, data):
+        articulo = Articulos.objects.filter(pk=data)
+        if not articulo:
+            raise serializers.ValidationError(f'Articulos with pk: {data} does not exists.')
+        return articulo
 
 
-class ReadFields(serializers.Serializer):
-    tipo_conexion = serializers.ListField()
-    materiales = serializers.ListField()
-    articulos = serializers.ListField()
+    def create(self, validated_data, *args, **kwargs):
+        file = validated_data.pop('file')
+        expediente = Expediente(
+            **validated_data,
+        )
+        expediente.save()
+
+        ExpedientesImagenes(
+            file=file,
+            expediente=expediente,
+        ).save()
+
+        return expediente
+
+
+class ExpedienteListSerializer(serializers.ModelSerializer):
+    pass
--- backend/services/api/views.py
+++ backend/services/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateMode
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.authtoken.models import Token
+from rest_framework.parsers import MultiPartParser
 
 # Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -13,7 +14,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 # Permissions
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .serializers import ReadFields
+from .serializers import ExpedienteSerializer, ExpedienteListSerializer
+from .paginations import CustomPagination
 
 from backend.services.models import Articulos, TiposGestion, Material, Expediente
 
@@ -22,24 +24,65 @@ from backend.services.api import serializers
 
 User = get_user_model()
 
-
 class ReadFieldsViewset(viewsets.ViewSet):
-    serializer_class = ReadFields
-    # queryset = Expediente.objects.all()
-    
     def list(self, request, *args, **kwargs):
         material = Material.objects.values('id', 'name', 'slug_name')
         tipo_conexion = TiposGestion.objects.values('id', 'name', 'slug_name')
         articulos = Articulos.objects.values('id', 'name', 'slug_name', 'precio_base')
 
-        # serializer = ReadFields(
-        #     tipo_conexion=tipo_conexion,
-        #     material=material,
-        #     articulos=articulos
-        # )
         return Response({
             "materiales": material,
             "tipo_conexion": tipo_conexion,
             "articulos": articulos
         })
-    
+
+
+class ExpedienteViewset(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    parser_classes = (MultiPartParser,)
+    permission_classes = [AllowAny]
+    queryset = Expediente.objects.all()
+    lookup_field = "pk"
+    pagination_class = CustomPagination
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('trabajador__username', "id")
+
+    def get_permissions(self):
+        """Assign permissions for each action"""
+        permissions = [IsAuthenticated]
+        return [permision() for permision in permissions]
+
+    def get_serializer_class(self):
+        """Return serializer based on action."""
+        action_mappings = {
+            'create': ExpedienteSerializer,
+        }
+        return action_mappings.get(self.action, ExpedienteListSerializer)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        _serializer = self.perform_create(serializer)
+        return Response(
+            {'expediente': _serializer},
+            status=status.HTTP_201_CREATED
+        )
+
+    def perform_create(self, serializer):
+        vehicle_workday = serializer.save(trabajador=self.request.user)
+        return ExpedienteListSerializer(vehicle_workday).data
+
+    def perform_update(self, serializer):
+        serializer.save(worker=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
